e8e37a0c0fce25299d880efbb3d905ea3323a400
Bibi138 <fabio.lourenco@protonmail.com>
2024-11-29 13:57:43 +0100

    loading page done

--- my-app/app/Photos/loading.tsx
+++ my-app/app/Photos/loading.tsx
@@ -1,22 +1,21 @@
-import React from 'react'
-import Image from "next/image"
-//import loadingSpirale from "/public/loading-spirale-2.jpg";
-import loadingSpirale from "/public/loading-spirale.jpg";
+import React from 'react';
+import Image from "next/image";
+import loadingSpirale from "/public/loading-spirale-modified.png";
 
 const Loading = (): JSX.Element => {
   return (
-    <div className="w-full min-h-screen">
-        <div className='w-[200px] flex items-center p-4'>
+    <div className="flex items-center justify-center w-full min-h-screen">
+        <div className='w-full h-screen flex flex-col items-center jsutify-center p-4'>
 
           <Image
               src={loadingSpirale}
-              width={20}
-              height={20}
+              width={450}
+              height={450}
               alt="img loader"
-              className="object-cover animate-spin" 
+              className="object-cover animate-spin m-auto opacity-50" 
           />
 
-          <h2 className='font-bold ml-4'>Loading...</h2>
+          <h2 className='text-slate-500 font-bold ml-4'>Loading...</h2>
 
         </div>
     </div>
