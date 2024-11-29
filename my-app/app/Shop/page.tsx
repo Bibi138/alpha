9637283c3a9160a4a053b368115033ca559c9b1d
Bibi138 <fabio.lourenco@protonmail.com>
2024-11-29 14:50:44 +0100

    color improved

--- my-app/app/Shop/page.tsx
+++ my-app/app/Shop/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 const Shop = (): JSX.Element => {
     return (
         <div className="w-full min-h-screen">
-            <h1 className="text-3xl font-bold text-slate-700 pt-24 pl-4">Shop</h1>
+            <h1 className="text-3xl font-bold text-slate-500 pt-24 pl-4">Shop</h1>
 
             <main className="flex flex-col items-center justify-content w-full h-screen">
                 <h3 className="text-5xl font-bold text-slate-500 bg-slate-50 m-auto mt-[200px]">! En maintenance !</h3>
