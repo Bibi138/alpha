83c3826baf53d5dd44f605cc16df676c555b50ab
Bibi138 <fabio.lourenco@protonmail.com>
2024-12-06 15:03:34 +0100

    style improved & new font added

--- my-app/app/components/Footer.tsx
+++
@@ -1,8 +0,0 @@
-const Footer = (): JSX.Element => {
-    return (
-        <footer className="absolute z-10 bottom-0 flex flex-col items-center jsutify-center w-full h-[70px] text-slate-50 bg-slate-900">
-            <p className="m-auto">Fabio Lourenço © 2024</p>
-        </footer>
-    )
-}
-export default Footer;
